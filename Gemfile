9defd9a7ce733ad3412329912190e10d83b1c32b
kimberley <yoyijun@gmail.com>
2017-11-26 17:46:57 +0800

    use mysql2

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'sqlite3', group: :development
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
@@ -61,6 +61,10 @@ group :development do
   gem 'capistrano'
   gem 'capistrano-rvm'
   gem 'capistrano-rails'
+end
+
+group :production do
+  gem "mysql2"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/deploy.rb
+++ config/deploy.rb
@@ -22,9 +22,12 @@ set :deploy_to, "/home/ubuntu/meet_dog"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
